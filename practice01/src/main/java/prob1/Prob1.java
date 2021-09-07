15d88789ec4c055fa75bbc9a1f82b509d55043d7
user <user@61.83.118.69>
2021-09-07 09:59:56 +0900

    first commit

---
+++ practice01/src/main/java/prob1/Prob1.java
@@ -0,0 +1,20 @@
+package prob1;
+
+import java.util.Scanner;
+
+public class Prob1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner( System.in );
+		
+		int a = 100;
+		
+		if(a %= 3 == 0) {
+			System.out.println("3의 배수입니다.");
+		}
+		else {
+			System.out.println("3의 배수가 아닙니다.");
+		}
+		
+		scanner.close();
+	}
+}
